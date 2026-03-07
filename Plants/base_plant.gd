8076902569c590b5d0d80e1f12bba2522987369c
Camarão <camaraodev@gmail.com>
2026-03-07 14:22:39 +0000

    Fixed some small mistakes in base plant

--- Plants/base_plant.gd
+++ Plants/base_plant.gd
@@ -3,11 +3,11 @@ class_name Plant
 
 # TODO: 
 # Receive Item
-# Interacted with signal or function
-# Get Grown Function
+
 signal grew (growStage : int)
 @export var isByStages : bool = false
 @export var growTime : float = 1 ## Time to grow in seconds
+@export var growGoal : int = 3 ## How many stages the plant has
 @export_group("Nodes")
 @export var interactable : Interactable
 @export var sprite: AnimatedSprite2D
@@ -19,9 +19,8 @@ var GrowProgress : float: ## How far the plant is in the growing progress. from 
 	set(value):
 		growProgress = value
 		check_grow_progress()
+var growProgress: float = 1
 
-var growProgress: float = 0
-var growGoal : int = 2
 
 func _ready() -> void:
 	interactable.wasInteracted.connect(on_interacted)
@@ -36,9 +35,9 @@ func on_interacted(_interactor: Node2D):
 
 func _physics_process(delta: float) -> void:
 	if (!isByStages and GrowProgress < growGoal):
-		GrowProgress += delta / growTime
+		GrowProgress += (delta / growTime) * growGoal
 
 func check_grow_progress():
-	sprite.animation = "stage" +  str(int(GrowProgress))
+	sprite.animation = "stage" +  str(int(GrowProgress)-1)
 	if (growProgress >= growGoal):
 		isGrown = true
